1b03538edecd3313ab7674cf47918efcca168782
kkundan02 <166752836+kkundan02@users.noreply.github.com>
2024-07-11 12:28:18 +0530

    Update main.py updating api

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #main.py
-print("Hello, Kundan it's your GithubAction Demo Project")
+#print("Hello, Kundan it's your GithubAction Demo Project")
+import requests
+def call_api():
+    url = "https://api.example.com/data"
+    response = requests.get(url)
+    print(response.json())
+
+call_api()
